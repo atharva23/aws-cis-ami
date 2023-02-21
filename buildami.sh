583ead27f2e3c53716730246ccd85344efdd6f9a
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-02-21 16:01:41 +0530

    Update buildami.sh

--- buildami.sh
+++ buildami.sh
@@ -5,7 +5,9 @@ time_diff=$(( (current_date - $(date --date="$latest_ami_date" +%s)) / 86400 ))
 if [ $time_diff -lt 1 ]; then
   echo "Amazon Linux 2 AMI has changed in the last 1 day"
   packer validate -var-file=variables.json cis-ami.pkr.hcl
-  aws ssm put-parameter --name "/gemini/latest-ami" --value "$(packer build  -var-file=variables.json cis-ami.pkr.hcl | awk '/^us-east-1:/ {print $2}')" --type String --overwrite
+  ami_value=$(packer build  -var-file=variables.json cis-ami.pkr.hcl | awk '/^us-east-1:/ {print $2}')
+  echo $ami_value
+  aws ssm put-parameter --name "/gemini/latest-ami" --value $ami_value --type String --overwrite
 else
   echo "Amazon Linux 2 AMI has not changed in the last 1 day no need to build new image"
 fi
