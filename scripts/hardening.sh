4f0ceddd2b3cb9a50f13d2da7cd492af478a9b13
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-02-21 17:25:25 +0530

    Update hardening.sh

--- scripts/hardening.sh
+++ scripts/hardening.sh
@@ -3,9 +3,9 @@
 # This script does the AMI harening tasks by executing bash commands
 # We can't directly copy the conf files to "root" owned directories via packer file provisioned
 # So we put the files in /tmp and move them by executing this shell script
-cd ~
-sudo cp -r  ~/AMAZON-LINUX-2 ~/tmp/files
-sudo ansible-playbook -i /etc/ansible/hosts  ~/AMAZON-LINUX-2/site.yml --become
+
+#sudo cp -r  ~/AMAZON-LINUX-2 ~/tmp/files
+sudo ansible-playbook -i /etc/ansible/hosts  /tmp/AMAZON-LINUX-2/site.yml --become
 
 
 
